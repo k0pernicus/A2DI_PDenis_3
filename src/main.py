9b78bd97d6c697b45a7427dd9e609c4e48e3cf88
qw0te <baert.quentin@gmail.com>
2016-01-12 18:56:20 +0100

    Début du TP

---
+++ src/main.py
@@ -0,0 +1,23 @@
+#-*- coding: utf-8 -*-
+
+import numpy as np
+import math
+from sklearn import datasets
+
+def load_iris_data():
+    """
+    Méthode permettant de charger en mémoire les données IRIS, contenues dans scikit learn.
+    """
+
+    iris = datasets.load_iris()
+    X = iris.data[:, :4]
+    C = iris.target
+
+    return (X, C)
+
+def similarity(xi, xj, sigma=1):
+    """
+    Donne la similarité entre deux données.
+    """
+
+    return math.exp(-(np.linalg.norm(np.subtract(xi, xj))) / math.pow(sigma, 2))
