7926f90b11909f8ce37eb5efcfc8731e06bf97b4
k0pernicus <antonin.carette@gmail.com>
2016-01-17 22:35:10 +0100

    Ajout d'une fonction permettant de choisir 3 points aléatoires, de 3 classes différentes

--- src/main.py
+++ src/main.py
@@ -30,3 +30,19 @@ def construct_epsilon_graph(X):
     """
 
     return [[similarity(xi, xj) for xi in X] for xj in X]
+
+def choose_3_points_from(X, C):
+    """
+    Fonction permettant de choisir 3 points aléatoires - chacun étant d'une classe
+    inférieure à l'autre
+    """
+
+    r_0 = random.randrange(0, 50)
+    r_1 = random.randrange(50, 100)
+    r_2 = random.randrange(100, 150)
+
+    c_0 = (X[r_0], C[r_0])
+    c_1 = (X[r_1], C[r_1])
+    c_2 = (X[r_2], C[r_2])
+
+    return (c_0, c_1, c_2)
