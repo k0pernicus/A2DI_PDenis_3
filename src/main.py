f0172c43c48c8eb0d13b2aefd8fab1d8f3039ec0
k0pernicus <antonin.carette@gmail.com>
2016-01-18 21:07:15 +0100

    Modification de la fonction permettant de prendre 3 valeurs aléatoires -> retourne un nouveau vecteur contenant, pour les 3 premières valeurs, les 3 valeurs prises aléatoirement

--- src/main.py
+++ src/main.py
@@ -37,12 +37,31 @@ def choose_3_points_from(X, C):
     inférieure à l'autre
     """
 
+    # Génère des nombres aléatoires, chacun dans des intervalles permettant
+    # de choisir 1 point pour chaque classe
     r_0 = random.randrange(0, 50)
     r_1 = random.randrange(50, 100)
     r_2 = random.randrange(100, 150)
 
-    c_0 = (X[r_0], C[r_0])
-    c_1 = (X[r_1], C[r_1])
-    c_2 = (X[r_2], C[r_2])
+    # Prendre les valeurs -> vecteur X
+    c_0 = X[r_0]
+    c_1 = X[r_1]
+    c_2 = X[r_2]
 
-    return (c_0, c_1, c_2)
+    # print("c_0 {}".format(c_0))
+    # print("c_1 {}".format(c_1))
+    # print("c_2 {}".format(c_2))
+
+    # Suppression des valeurs
+    X = np.delete(X, r_0, 0)
+    X = np.delete(X, r_1, 0)
+    X = np.delete(X, r_2, 0)
+
+    # Ajout des valeurs dans les 3 premières positions du nouveau vecteur X
+    X = np.insert(X, 0, c_0, axis=0)
+    X = np.insert(X, 1, c_1, axis=0)
+    X = np.insert(X, 2, c_2, axis=0)
+
+    # print("X {}".format(X))
+
+    return X
